61e451d2929999ecb23203a7621ab2e71980ac1f
Dereku <dereku@default.im>
2018-06-03 12:49:45 +0700

    Fixed NPE that reported in rubukkit.org/posts/1540747

--- src/ru/endlesscode/markitem/PlayerListener.java
+++ src/ru/endlesscode/markitem/PlayerListener.java
@@ -26,7 +26,7 @@ class PlayerListener implements Listener {
 
         List<ItemStack> armorList = new ArrayList<>();
         for (ItemStack armor : player.getInventory().getArmorContents()) {
-            if (armor.getType() == Material.AIR || !MarkItem.getItemMarker().hasMark(armor)) {
+            if (armor == null || armor.getType() == Material.AIR || !MarkItem.getItemMarker().hasMark(armor)) {
                 armorList.add(new ItemStack(Material.AIR, 0));
             } else {
                 armorList.add(MarkItem.getItemMarker().hasOldMark(armor) ? MarkItem.getItemMarker().updateMark(armor) : armor);
